f04bce6d292b2e8455297a1920d405b0d7399004
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-26 17:20:42 -0300

    update

--- pages/api/produtos.js
+++ pages/api/produtos.js
@@ -1,9 +1,14 @@
+function numeroAleatorios(min = 1, max = 10000){
+  return parseInt(Math.random() * (max - min)) + min
+}
+
+
 export default function produtos(req, res) {
   res.status(200).json([
-    {id: 1, nome: "Caneta", preco: 5.60},
-    {id: 2, nome: "Caderno", preco: 15},
-    {id: 3, nome: "Lapis", preco: 2.00},
-    {id: 4, nome: "Livro", preco: 45},
-    {id: 5, nome: "Régua", preco: 1.50},
+    {id: numeroAleatorios(), nome: "Caneta", preco: 5.60},
+    {id: numeroAleatorios(), nome: "Caderno", preco: 15},
+    {id: numeroAleatorios(), nome: "Lapis", preco: 2.00},
+    {id: numeroAleatorios(), nome: "Livro", preco: 45},
+    {id: numeroAleatorios(), nome: "Régua", preco: 1.50},
   ])
 }
--- pages/dinamico2.jsx
+++ pages/dinamico2.jsx
@@ -1,10 +1,4 @@
-//consumindo API com o metodo getStaticProps e utilizando a função async
-
-//o método getStaticProps só é executado no lado no servidor
-
-//conectado com /pages/api/produtos
-
-export async function getStaticProps() {
+export async function getServerSideProps() {
   console.log("[server]gerando props para o componente...");
 
   const resp = await fetch("http://localhost:3000/api/produtos");
@@ -17,21 +11,22 @@ export async function getStaticProps() {
   }
 }
 
-export default function Estatico04(props){
+export default function Dinamico2(props){
   console.log("[Cliente] Renderizando o componente");
   
   function renderizarProdutos() {
     return props.produtos.map(produto => {
-      return 
-      <li key={produto.id}>
-        {produto.nome} tem preço de R${produto.preco}
-      </li>
+      return (
+        <li key={produto.id}>
+          {produto.id} - {produto.nome} tem preço de R${produto.preco}
+        </li>
+      )  
     })
   }
 
   return (
     <div>
-      <h1>Estático #04</h1>
+      <h1>Dinâmico #02</h1>
       <ul>
         {renderizarProdutos()}
       </ul>
