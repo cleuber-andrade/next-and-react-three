f4aac64c6c8660d82a323e27d6ff1bc7f117460f
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-26 16:18:51 -0300

    Update estatico04.jsx

--- pages/estatico04.jsx
+++ pages/estatico04.jsx
@@ -6,6 +6,7 @@
 
 export async function getStaticProps() {
   console.log("[server]gerando props para o componente...");
+
   const resp = await fetch("http://localhost:3000/api/produtos");
   const produtos = await resp.json();
 
@@ -18,6 +19,7 @@ export async function getStaticProps() {
 
 export default function Estatico04(props){
   console.log("[Cliente] Renderizando o componente");
+  
   function renderizarProdutos() {
     return props.produtos.map(produto => {
       return <li key={produto.id}>{produto.nome} tem preço de R${produto.preco}</li>
