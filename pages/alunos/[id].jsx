bbe3e375acfe627c3e8a53f151c9ba60f52d7d66
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-26 18:23:24 -0300

    update

--- pages/alunos/[id].jsx
+++ pages/alunos/[id].jsx
@@ -1,19 +1,22 @@
 //quando o fallback for false ele vai gerar um erro 404 de pagina não encontrada o id específico que foi colocado dentro do paths
 
 
-export function getStaticPaths(){
+export async function getStaticPaths(){
+  const resp = await fetch(`http://localhost:3000/api/alunos/tutores`);
+  const ids = await resp.json();
+
+  const paths = ids.map(id =>{
+    return{ params: {id}}
+  }) 
+
   return {
-    fallback: false,
-    paths: [
-      {params: { id: "107"}},
-      {params: { id: "203"}},
-      {params: { id: "1346"}},
-    ]
+    fallback: false, //404
+    paths
   }
 }
 
-export async function getStaticProps(context){
-  const resp = await fetch(`http://localhost:3000/api/alunos/${context.params.id}`);
+export async function getStaticProps({ params }){
+  const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`);
   const aluno = await resp.json();
   
   return {
---
+++ pages/api/tutores.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.status(200).json([
+    100,
+    200, 
+    300,
+    400,
+  ])
+}
