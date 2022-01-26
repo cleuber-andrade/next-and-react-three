192675a9d2fb976417e7370788e56ee824d14991
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-26 13:08:58 -0300

    update

---
+++ pages/api/produtos.js
@@ -0,0 +1,8 @@
+export default function produtos(req, res) {
+  res.status(200).json([
+    {id: 1, nome: "Caneta", preco: 5.60},
+    {id: 2, nome: "Caderno", preco: 15},
+    {id: 3, nome: "Lapis", preco: 2.00},
+    {id: 4, nome: "Livro", preco: 45},
+  ])
+}
